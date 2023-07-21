e02959e7af5b54ea5f36b152e5c8c70a5b15799d
iwoeix <139027984+iwoeix@users.noreply.github.com>
2023-07-21 17:55:26 +0800

    Update all.hpp
    Now instead of pasting the entire code to all.hpp, you can just include the file that contains the code.

--- all.hpp
+++ all.hpp
@@ -1,15 +1,1 @@
-template <class base, class exp>
-base fpow(base a, exp b)
-{
-    base res = 1 % p;
-    for (; b; b & 1 && (res *= a), a *= a, b >>= 1);
-    return res;
-}
-
-template <class base, class exp, class mod>
-base fpow(base a, exp b, mod p)
-{
-    base res = 1 % p;
-    for (; b; b & 1 && ((res *= a) %= p), (a *= a) %= p, b >>= 1);
-    return res;
-}
+#include "fpow.hpp"
